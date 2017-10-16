c7251e98562467a0c9ec92457b888554b689304b
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-10-17 03:16:19 +0900

    impl creating project

--- config.ml
+++ config.ml
@@ -3,3 +3,7 @@ let version = "0.1"
 let debug_mode = ref false
 
 let verbose_mode = ref false
+
+let default_config_file = "Yscafile"
+
+let config_file = ref default_config_file
--- main.ml
+++ main.ml
@@ -1,9 +1,11 @@
 open Core.Std
 open Printf
 
-let configure ~debug ~verbose =
+let configure ?config_file ~debug ~verbose () =
   Config.debug_mode := debug;
-  Config.verbose_mode := verbose
+  Config.verbose_mode := verbose;
+  Option.iter config_file ~f:(fun file -> Config.config_file := file);
+  ()
 
 let basic_spec () =
   Command.Spec.(
@@ -15,23 +17,38 @@ let basic_spec () =
 let subcmd_new =
   Command.basic
     ~summary:"create a new project"
-    (basic_spec ())
-    (fun debug verbose () ->
-       configure ~debug ~verbose;
+    Command.Spec.(
+      (basic_spec ())
+      +> anon ("name" %: string)
     )
+    (fun debug verbose name () ->
+       configure ~debug ~verbose ();
+       Main_new.run ~name)
+
+let subcmd_build =
+  Command.basic
+    ~summary:"compile source files to Go source files"
+    Command.Spec.(
+      (basic_spec ())
+      +> flag "-config" (required string) ~doc:"config file (default: Yscafile)"
+    )
+    (fun debug verbose config_file () ->
+       configure ~debug ~verbose ~config_file ();
+       ())
 
 let subcmd_clean =
   Command.basic
     ~summary:"remove built files"
     (basic_spec ())
     (fun debug verbose () ->
-       configure ~debug ~verbose;
+       configure ~debug ~verbose ();
     )
 
 let main =
   Command.group
     ~summary:"Esca build tool"
     [("new", subcmd_new);
+     ("build", subcmd_build);
      ("clean", subcmd_clean)]
 
 let () =
